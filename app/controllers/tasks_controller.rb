ea7e1ef357507f3d87f112bf431c6b3dab9b2c6f
Ting <0917355950teresa@gmail.com>
2021-02-16 15:13:14 +0800

    task edit&update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :edit, :update]
   
   def index
     @tasks = Task.all
@@ -12,13 +12,24 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to root_path, notice: "任務新增成功"
+      redirect_to root_path, notice: "任務新增成功！"
     else
       render :new
     end
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path, notice: "任務更新成功！"
+    else
+      render :edit
+    end
   end
 
   private
