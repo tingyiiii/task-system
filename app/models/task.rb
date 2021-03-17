3f5f06c3473a09881e93db8f1070562e6f2fe09c
Ting <0917355950teresa@gmail.com>
2021-03-17 14:47:49 +0800

    update feature test -> order by Task end time

--- app/models/task.rb
+++ app/models/task.rb
@@ -19,7 +19,7 @@ class Task < ApplicationRecord
   def end_time_after_start_time
     return if end_time.blank? || start_time.blank?
  
-    if end_time < start_time
+    if end_time <= start_time
       errors.add(:end_time, I18n.t('tasks.end_time_after_start_time')) 
     end
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -13,5 +13,10 @@ RSpec.describe Task, type: :model do
     it { is_expected.to define_enum_for(:status).with_values(%w(pending processing completed)) }
     it { is_expected.to validate_presence_of(:start_time) }
     it { is_expected.to validate_presence_of(:end_time) }
+    it 'validates the task end time is after start time' do
+      task = Task.new(start_time: Faker::Time.forward, end_time: Time.now)
+      task.validate
+      expect(task.errors[:end_time]).to include("#{I18n.t('tasks.end_time_after_start_time')}")
+    end
   end
 end
